5cbea615f129793640ae6482f72ba07b67a25732
titouan <ttn.glock@orange.fr>
2020-05-25 14:51:47 +0100

    first component
    first component with and without jsx syntax

--- memory/src/index.js
+++ memory/src/index.js
@@ -11,6 +11,46 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//creation d'un composant
+function CoolComponent() {
+  //retourne un DOM virtuel react grace a react.createElement
+  /*
+  Le premier element correspond au nom du composant
+  le deuxieme correspond a une serie d'attribut
+  le troisieme correspond au contenu à afficher
+  */
+  return React.createElement('p', {}, 'hello world !!!')
+}
+
+//permet d'afficher le composant dans la page web
+ReactDOM.render(
+  React.createElement(CoolComponent),
+  document.getElementById('root')
+)
+//----------------------------------------
+
+//creation d'un composant pour le h1
+function FirstTitle(){
+  return React.createElement('h1', {}, 'First title')
+}
+
+ReactDOM.render(
+  React.createElement(FirstTitle),
+  document.getElementById('titre')
+)
+//---------------------------------------
+
+//creation d'un composant avec jsx
+function ComponentJsx(){
+  return <p>jsx component</p>
+}
+
+ReactDOM.render(
+  <ComponentJsx />,
+  document.getElementById('jsxComponent')
+)
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
